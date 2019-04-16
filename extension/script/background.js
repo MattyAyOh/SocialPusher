c4e57c6090b6ffca067491b23f2a4ccb4ab72d01
Matt Ao <matt@bettylabs.io>
2019-04-15 23:47:43 -0700

    give popup the ability to pull title from background page

---
+++ extension/script/background.js
@@ -0,0 +1,2 @@
+//Need a script for the "background" key in the manifest. 
+//This allows us to reference `chrome.extension.getBackgroundPage()` in popup.js
--- extension/script/popup.js
+++ extension/script/popup.js
@@ -2,9 +2,26 @@
 $(function() {
   $('#pushButton').click(function(e) {
     e.preventDefault();
-    alert("testing "+ $('#streamIDInput').val())
-    window.close();
+    alert("testing "+ $('#streamIDInput').val());
+
+    const Http = new XMLHttpRequest();
+    const url='https://ptsv2.com/t/mightyducks123/post';
+    Http.open("POST", url);
+    Http.send();
+    Http.onreadystatechange=(e)=>{
+    alert(Http.responseText);
+    }
   });
 });
 
+// Inject the payload.js script into the current tab after the popout has loaded
+window.addEventListener('load', function (evt) {
+	chrome.extension.getBackgroundPage().chrome.tabs.executeScript(null, {
+		file: 'payload.js'
+	});;
+});
 
+// Listen to messages from the payload.js script and write to popout.html
+chrome.runtime.onMessage.addListener(function (message) {
+	document.getElementById('payloadText').innerHTML = message;
+});
